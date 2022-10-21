12c0c9857312b4a67be38776a0c59ac1ff57698d
jessefrance <jesseluis09@gmail.com>
2022-10-21 20:33:32 -0300

    Finished commit in script.js

--- scripts/script.js
+++ scripts/script.js
@@ -6,11 +6,11 @@ class StarRating {
     
     constructor(qs) {
         this.ratings = [
-            {id: 1, name: "Terrible"};
-            {id: 2, name: "Bad"};
-            {id: 3, name: "Ok"};
-            {id: 4, name: "Good"};
-            {id: 5, name: "Exellent"};
+            {id: 1, name: "Terrible"},
+            {id: 2, name: "Bad"},
+            {id: 3, name: "Ok"},
+            {id: 4, name: "Good"},
+            {id: 5, name: "Exellent"},
         ];
 
         this.rating = null;
@@ -30,5 +30,32 @@ class StarRating {
 		}
     }
 
-    
+    updateRating(e) {
+
+        Array.from(this.el.querySelector (`[for*="rating"]`)).forEach(el=> {
+            el.className = "rating__label";
+        })
+
+        const ratingObject = this.ratings.find(r => r.id === +e.target.value);
+		const prevRatingID = this.rating?.id || 0;
+
+        let delay = 0;
+		this.rating = ratingObject;
+		this.ratings.forEach(rating => {
+			const { id } = rating;
+
+            const ratingLabel = this.el.querySelector(`[for="rating-${id}"]`);
+
+			if (id > prevRatingID + 1 && id <= this.rating.id) {
+				++delay;
+				ratingLabel.classList.add(`rating__label--delay${delay}`);
+			} 
+            const ratingTextEl = this.el.querySelector(`[data-rating="${id}"]`);
+
+			if (this.rating.id !== id)
+				ratingTextEl.setAttribute("hidden",true);
+			else
+				ratingTextEl.removeAttribute("hidden");
+        });
+    }
 }
